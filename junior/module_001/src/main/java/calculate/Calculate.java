9cd3970c184079ecb301073806ad3ae6f524e989
Tatyana <sharkka@mail.ru>
2018-05-29 10:23:40 +0300

    java doc

--- junior/module_001/src/main/java/calculate/Calculate.java
+++ junior/module_001/src/main/java/calculate/Calculate.java
@@ -1,6 +1,26 @@
+/**
+* Calculate
+* @author Vakhtangova Tatyana (sharkka@mail.ru)
+*/
+
 class Calculate {
+
+    /**
+    * Main.
+    * @param  args - args
+    */
     public static void main(String[] args) {
         System.out.println("Hello World!");
 	System.out.println("Hello World!");
     }
+    /**
+    * Echo
+    * @param  name your name
+    * @return Echo plus your name
+    */
+
+    public String echo(String name) {
+	System.out.println(name);
+	return "Echo"+ name;
+    }	
 }
---
+++ junior/module_001/src/main/java/calculate/package-info.java
@@ -0,0 +1,6 @@
+/**
+*
+* @author Vakhtangova Tatyana (sharkka@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
