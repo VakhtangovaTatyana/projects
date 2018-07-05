ddb975b06820cf925e73a065f31a8387f5558329
Tatyana <sharkka@mail.ru>
2018-07-05 10:27:46 +0300

    refactor Paint

--- junior/module_001/src/main/java/ru/job/loop/Paint.java
+++ junior/module_001/src/main/java/ru/job/loop/Paint.java
@@ -1,21 +1,20 @@
 package ru.job.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * @author Vakhtangova Tatyana (sharkka@mail.ru)
  */
 public class Paint {
-    /**
-     * @param height высота
-     * @return result пирамиду из символов ^ и пробелов
-     */
-    public String rightTriangle(int height) {
+
+
+    //Java Generics
+    private String loopBy(int height, int width, BiPredicate<Integer, Integer> predicat) {
         //Буфер для результата
         StringBuilder screen = new StringBuilder();
-        //ширина равна высоте
-        int width = height;
         for (int row = 0; row < height; row++) {//row - стркоа
             for (int column = 0; column < width; column++) {
-                if (row >= column) {
+                if (predicat.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
@@ -23,46 +22,23 @@ public class Paint {
             }
             screen.append(System.lineSeparator());
         }
-
         return screen.toString();
     }
 
+    public String rightTriangle(int height) {
+        return this.loopBy(height, height, (row, column) -> row >= column); // лямбда функция = анонимная функция
+    }
 
     public String leftTriangle(int height) {
-        //Буфер для результата
-        StringBuilder screen = new StringBuilder();
-        //ширина равна высоте
-        int width = height;
-        for (int row = 0; row < height; row++) {//row - стркоа
-            for (int column = 0; column < width; column++) {
-                if (row >= width - column - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-
-        return screen.toString();
+        return this.loopBy(height, height, (row, column) -> row >= height - column - 1); // лямбда функция = анонимная функция
     }
 
     public String piramid(int height) {
-        //Буфер для результата
-        StringBuilder screen = new StringBuilder();
-        //ширина равна высоте
-        int width = height * 2 - 1;
-        for (int row = 0; row < height; row++) {//row - стркоа
-            for (int column = 0; column < width; column++) {
-                if (row >= height - column - 1 && column <= ((height * 2 - 1) / 2 + row)) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-
-        return screen.toString();
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && column <= ((height * 2 - 1) / 2 + row)
+        ); // лямбда функция = анонимная функция
     }
+
 }
